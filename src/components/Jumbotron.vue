eb1c06b64bd4344db0b2a7aba1bad50a4706bda6
La-wii <sentilavale@gmail.com>
2021-08-01 16:58:46 +0200

    jumbotron

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="jumbotron">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Jumbotron",
+}
+</script>
+
+<style scoped lang="scss">
+    .jumbotron{
+        height: 500px;
+        background: linear-gradient(180deg, rgba(151,41,57,1) 0%, rgba(154,57,71,1) 35%, rgba(208,93,110,1) 100%);
+    }
+</style>
