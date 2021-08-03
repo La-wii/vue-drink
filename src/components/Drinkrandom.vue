d92596e9fee322ef1ca57650e379eccfdb26f15e
La-wii <sentilavale@gmail.com>
2021-08-03 16:05:30 +0200

    fixed card

--- src/components/Drinkrandom.vue
+++ src/components/Drinkrandom.vue
@@ -2,24 +2,42 @@
     <div class="drink-content pt-5">
         <div class="container-lg pt-5">
             <div class="row">
-                <div class="col-xs-12 col-md-6">
-                    <div v-for="(element, index) in drink" :key="index" class="card mb-3" style="max-width: 540px;"> 
+                <div class="col-xs-12 col-md-6" id="drinkrandom">
+                    <div v-for="(element, index) in drink" :key="index" class="card mb-3 text-center" style="max-width: 540px;"> 
                         <img :src="element.strDrinkThumb" class="img-fluid rounded-start" alt="...">                                
                         <div class="card-body">
-                            <h4 class="card-title pb-3">{{element.strCategory}}</h4>
+                            <h4 class="card-title pb-3">
+                                <strong>
+                                    {{element.strDrink}}
+                                </strong>                               
+                            </h4>
+                            <h4 class="card-title pb-3">
+                                <strong>
+                                    {{element.strCategory}}
+                                </strong>   
+                            </h4>
+                            <h5>
+                                <strong>
+                                    Categoria: 
+                                </strong>  
+                            </h5>
                             <p class="card-text">                               
-                                <span>Categoria: </span>
-                                <br>
                                 {{element.strCategory}}                             
                             </p>
+                            <h5>
+                                <strong>
+                                    Bicchiere:  
+                                </strong>  
+                            </h5>
                             <p class="card-text">
-                                <span>Bicchiere: </span> 
-                                <br>
                                 {{element.strGlass}}                                
                             </p>
+                            <h5>
+                                <strong>
+                                    Ingredienti: 
+                                </strong>  
+                            </h5>
                             <p class="card-text">
-                                <span>Ingredienti: </span>
-                                <br>
                                 {{element.strIngredient1}}
                                 <br>
                                 {{element.strIngredient2}}
@@ -34,8 +52,13 @@
                                 <br>
                                 {{element.strIngredient7}}
                             </p>
+                            <h5>
+                                <strong>
+                                    Ricetta: 
+                                </strong>  
+                            </h5>
                             <p class="card-text">
-                                <span>Ricetta: </span> {{element.strInstructionsIT}}
+                                {{element.strInstructionsIT}}
                             </p>
                             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                         </div>
@@ -44,7 +67,10 @@
 
                 <div class="col-xs-12 col-md-6">
                     <h2>Un drink ad ogni refresh</h2>
-                    <p>
+                    <p class="text-light">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quis vitae voluptatem blanditiis repudiandae deleniti sed sunt nemo similique. Nam reprehenderit magnam dicta debitis, tempore illo repudiandae ea itaque optio?
+                        <br>
+                        <br>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quis vitae voluptatem blanditiis repudiandae deleniti sed sunt nemo similique. Nam reprehenderit magnam dicta debitis, tempore illo repudiandae ea itaque optio?
                         <br>
                         <br>
